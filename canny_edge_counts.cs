0be1ee86ece5a5163ac58c864b485837c0a78a57
SILENCER45 <102silencer@gmail.com>
2023-05-11 20:30:43 +0800

    dll pos fixe

--- canny_edge_counts.cs
+++ canny_edge_counts.cs
@@ -7,7 +7,7 @@ namespace autofocus_csharp
     {
         public static void Initializer()
         {
-            string pythonDll = @"C:\Users\ryanr\AppData\Local\Programs\Python\Python310\python310.dll";
+            string pythonDll = @"python310.dll";
             Environment.SetEnvironmentVariable("PYTHONNET_PYDLL", pythonDll);
             PythonEngine.Initialize();
         }
@@ -16,8 +16,8 @@ namespace autofocus_csharp
             Initializer();
             using (Py.GIL())
             {
-                dynamic smooth = Py.Import("smoothing");
-                smooth.bilateral_blur(photo_path);
+                dynamic np = Py.Import("numpy");
+                Console.WriteLine(np.cos(np.pi * 2));
             }
         }
     }
